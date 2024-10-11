d0a70e0525c8ec584e6cd00b9f0bd21312d74507
Technical_Isopod_075 <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2024-10-11 19:32:08 +0530

    Add files via upload

---
+++ NewOne/login.js
@@ -0,0 +1,18 @@
+// login.html
+document.getElementById('login-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    if (!email || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      alert("Invalid email format. Please use a valid email address.");
+      window.location.href = '404.html';
+    } else if (!password) {
+      window.location.href = '404.html';
+    } else {
+      // You can add your login logic here
+      // For now, it will just redirect to the 404.html page
+      window.location.href = '404.html';
+    }
+  });
+  
